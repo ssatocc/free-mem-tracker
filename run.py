7897c1e2dc63c332d139aed630c99318411cdd8e
ssatocc <ssatocc@gmail.com>
2024-02-18 15:38:27 +0900

    Update run.py

--- run.py
+++ run.py
@@ -3,21 +3,21 @@ import matplotlib.pyplot as plt
 
 
 def main():
-    data = pd.read_csv('output.csv')
-    data['date'] = pd.to_datetime(data['date'])
+    data = pd.read_csv("output.csv")
+    data["date"] = pd.to_datetime(data["date"])
 
     plt.figure(figsize=(10, 6))
-    plt.plot(data['date'], data['free-mem'])
+    plt.plot(data["date"], data["free-mem"])
 
-    plt.title('Free Memory')
-    plt.xlabel('Time')
-    plt.ylabel('Free Memory (MB)')
+    plt.title("Free Memory")
+    plt.xlabel("Time")
+    plt.ylabel("Free Memory (MB)")
     plt.grid(True)
     plt.xticks(rotation=90)
     plt.tight_layout()
 
-    plt.savefig('output.png')
+    plt.savefig("output.png")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
