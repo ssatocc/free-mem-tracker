99b4484876b5a2a5d645553aaf1f28d366844103
ssatocc <ssatocc@gmail.com>
2024-02-18 11:55:30 +0900

    Create generate_graph.py

---
+++ generate_graph.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+def main():
+    data = pd.read_csv('output.csv')
+    data['date'] = pd.to_datetime(data['date'])
+
+    plt.figure(figsize=(10, 6))
+    plt.plot(data['date'], data['free-mem'], marker='o', linestyle='-')
+
+    plt.title('Free Memory')
+    plt.xlabel('Time')
+    plt.ylabel('Free Memory (MB)')
+    plt.grid(True)
+    plt.xticks(rotation=90)
+    plt.tight_layout()
+
+    plt.savefig('output.png')
+
+
+if __name__ == '__main__':
+    main()
